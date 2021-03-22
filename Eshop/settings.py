01bbcb9ac02b71a4c1a135516ef8f775d445edf4
rushi.jagdale <rjcom99@gmail.com>
2021-03-22 11:57:07 +0530

    adding files

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -121,6 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
 MEDIA_URL ="/image/download/"
 MEDIA_ROOT = BASE_DIR
+
